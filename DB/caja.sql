cf9b9bf6720c58129e25496fe9ae190838a96110
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-02-20 07:27:52 +0100

    update
    update DB

--- DB/caja.sql
+++ DB/caja.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versione server:              8.0.32 - MySQL Community Server - GPL
+-- Versione server:              8.0.31 - MySQL Community Server - GPL
 -- S.O. server:                  Win64
 -- HeidiSQL Versione:            12.3.0.6589
 -- --------------------------------------------------------
@@ -14,7 +14,24 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura del database caja
+CREATE DATABASE IF NOT EXISTS `caja` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `caja`;
+
+-- Dump della struttura di tabella caja.caja
+CREATE TABLE IF NOT EXISTS `caja` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tamano` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cantidad` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dump dei dati della tabella caja.caja: ~2 rows (circa)
+REPLACE INTO `caja` (`id`, `tipo`, `tamano`, `cantidad`) VALUES
+	(1, 'R', '120x100x30', '50'),
+	(2, 'P', '150x60x29', '50');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
